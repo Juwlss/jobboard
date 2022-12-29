8e3b6aee3fa25fc814a68a1e168933a03eeb3409
Juwlss <98637159+Juwlss@users.noreply.github.com>
2022-12-29 22:54:47 +0800

    designs

--- django/jobportal/jobs/views.py
+++ django/jobportal/jobs/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import SubmitJob, ApplyJob
 
 # Create your views here.
 
 
 def index (request):
-    return HttpResponse("Hello World")
+    template = "index.html"
+    jobs = SubmitJob.objects.all()
+    context = {
+        'jobs': jobs,
+        'title': 'Find a Job',
+    }
+    return render(request, template,context)
+
+def detail(request, job_id):
+    return HttpResponse(f"You are looking for job {job_id}")
