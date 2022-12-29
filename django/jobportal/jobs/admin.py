0389ce09e82668d3bc55c03a8bef02470511dce8
Juwlss <98637159+Juwlss@users.noreply.github.com>
2022-12-29 17:47:17 +0800

    models updated

--- django/jobportal/jobs/admin.py
+++ django/jobportal/jobs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import SubmitJob, ApplyJob
 
 # Register your models here.
+
+admin.site.register(SubmitJob)
+admin.site.register(ApplyJob)
--- django/jobportal/jobs/models.py
+++ django/jobportal/jobs/models.py
@@ -1,21 +1,24 @@
 from django.db import models
+from django.db.models import Model
+
 
 # Create your models here.
 
 class SubmitJob(models.Model):
-    company_name = models.CharField(max_length=255)
-    job_vacancy = models.CharField(max_length=255)
-    job_category = models.CharField(max_length=255)
-    pub_date = models.DateTimeField('date published')
-    job_description = models.CharField(max_length=255)
-    salary = models.IntegerField('salary')
-    location = models.CharField(max_length=255)
-    time_employment = models.CharField(max)
+    company_name = models.CharField(default='', max_length=255)
+    job_vacancy = models.CharField(default='', max_length=255)
+    job_category = models.CharField(default='', max_length=255)
+    pub_date = models.DateTimeField('date published', auto_now_add=False, blank=True)
+    job_description = models.CharField('Job Description',default='', max_length=255)
+    salary = models.IntegerField(default=0)
+    location = models.CharField('Location',default='',max_length=255)
+    time_employment = models.CharField(default='',max_length=200)
     
 
 class ApplyJob(models.Model):
-     employee_name = models.CharField(max_length=255)
-     employee_email = models.EmailField(max_length=255)
-     employee_website = models.EmailField(max_length=255)
-     employee_cv = models.FileField
-     employee_coverletter = models.CharField(max_length=255)
+    job = models.ForeignKey(SubmitJob, on_delete=models.CASCADE)
+    employee_name = models.CharField('Name', default='', max_length=255)
+    employee_email = models.EmailField('Email', default='', max_length=255)
+    employee_website = models.CharField('Website/link', default='', max_length=255)
+    employee_cv = models.FileField('Upload your CV here')
+    employee_coverletter = models.CharField('Coverletter', default='', max_length=255)
