c3a05742419950c3eedb1855ecf90e66b7c66ac7
Juwlss <98637159+Juwlss@users.noreply.github.com>
2022-12-30 13:00:18 +0800

    update the interface

--- django/jobportal/jobs/urls.py
+++ django/jobportal/jobs/urls.py
@@ -5,5 +5,13 @@ app_name = 'jobs'
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('index.html',views.index, name='index'),
+    path('jobs.html',views.jobs, name='jobs'),
+    path('candidate.html',views.candidate, name='candidate'),
+    path('job_details.html',views.job_details, name='job_details'),
+    path('elements.html',views.elements, name='elements'),
+    path('blog.html',views.blog, name='blog'),
+    path('single-blog.html',views.single_blog, name='single_blog'),
+    path('contact.html',views.contact, name='contact'),
     path('<int:job_id>/', views.detail, name='detail'),
 ]
--- django/jobportal/jobs/views.py
+++ django/jobportal/jobs/views.py
@@ -4,9 +4,80 @@ from .models import SubmitJob, ApplyJob
 
 # Create your views here.
 
+#home page
 
 def index (request):
     template = "index.html"
+    jobs = SubmitJob.objects.all()
+    context = {
+        'jobs': jobs,
+        'title': 'Find a Job',
+    }
+    return render(request, template,context)
+
+#job page
+def jobs (request):
+    template = "jobs.html"
+    jobs = SubmitJob.objects.all()
+    context = {
+        'jobs': jobs,
+        'title': 'Find a Job',
+    }
+    return render(request, template,context)
+
+#candidate page
+def candidate (request):
+    template = "candidate.html"
+    jobs = SubmitJob.objects.all()
+    context = {
+        'jobs': jobs,
+        'title': 'Find a Job',
+    }
+    return render(request, template,context)
+
+#job details page
+def job_details (request):
+    template = "job_details.html"
+    jobs = SubmitJob.objects.all()
+    context = {
+        'jobs': jobs,
+        'title': 'Find a Job',
+    }
+    return render(request, template,context)
+
+#elements page
+def elements (request):
+    template = "elements.html"
+    jobs = SubmitJob.objects.all()
+    context = {
+        'jobs': jobs,
+        'title': 'Find a Job',
+    }
+    return render(request, template,context)
+
+#blog page
+def blog (request):
+    template = "blog.html"
+    jobs = SubmitJob.objects.all()
+    context = {
+        'jobs': jobs,
+        'title': 'Find a Job',
+    }
+    return render(request, template,context)
+
+#single-blog page
+def single_blog (request):
+    template = "single-blog.html"
+    jobs = SubmitJob.objects.all()
+    context = {
+        'jobs': jobs,
+        'title': 'Find a Job',
+    }
+    return render(request, template,context)
+
+#contact page
+def contact (request):
+    template = "contact.html"
     jobs = SubmitJob.objects.all()
     context = {
         'jobs': jobs,
